abb4ea7fca06c5f7a5ce3467294a2d5305fa3a14
feravsar <34317690+feravsar@users.noreply.github.com>
2020-02-03 13:23:12 +0300

    OOP(TR)
    Nesne Yönemli Programlamaya giriş.

---
+++ Uyg3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Uyg3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=new int();
+            bool b = new bool();
+            //Console.WriteLine(b);
+
+            Zaman z1 = new Zaman();
+            //z1.zamaniYaz();
+
+            Zaman z2 = new Zaman(3, 3, 2006);
+            Zaman z3 = new Zaman();
+        }
+    }
+}
---
+++ Uyg3/Zaman.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg3
+{
+    class Zaman
+    {
+        int gun;
+        int ay;
+        int yil;
+
+        public Zaman()
+        {
+            gun = DateTime.Now.Day;
+            ay = DateTime.Now.Month;
+            yil = DateTime.Now.Year;
+            //constructor sinif ismi ile ayni isimdedir
+            //bir deger return etmez
+            Console.WriteLine("parametresiz constr calisti");
+        }
+
+        public Zaman(int g, int a, int y)
+        {
+            gun = g;
+            ay = a;
+            yil = y;
+            //zamaniYaz();
+            Console.WriteLine("parametreli constr calisti");
+        }
+
+         static Zaman()
+        { 
+            //statik constructorlarda erisim seviyesi (public private gibi) belirtilmez
+            //statik constructor parametre almaz
+            Console.WriteLine("static constr calisti");
+             //statik constr. bir defa calisir
+             //butun constr.lardan once calisir.
+             //nesne turetilmezse calismaz
+        
+        }
+        public void zamaniYaz()
+        {
+            Console.WriteLine("{0}/{1}/{2}", gun, ay, yil);
+        }
+    }
+}
